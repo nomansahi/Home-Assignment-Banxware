ecaf178ebeac73cf131f57a1777ccea9d7f2841a
NOMAN ALI <87797326+nomansahi@users.noreply.github.com>
2023-09-11 17:27:13 +0200

    i removed the url from gethistoricalBalnce file because according to your requirements i need only to fetch the historical balnces

--- src/services/getHistoricalBalances.ts
+++ src/services/getHistoricalBalances.ts
@@ -5,22 +5,4 @@ import express = require("express");
 const axios = require("axios");
 
 const app = express();
-export function getHistoricalBalance() {
-  const BASE_URL =
-    "https://uh4goxppjc7stkg24d6fdma4t40wxtly.lambda-url.eu-central-1.on.aws";
-  const HEADERS = {
-    "x-api-key":
-      "b4a4552e-1eac-44ac-8fcc-91acca085b98-f94b74ce-aa17-48f5-83e2-8b8c30509c18",
-  };
-
-  app.get("/balances", async (req, res) => {
-    try {
-      const response = await axios.get(`${BASE_URL}/balances`, {
-        headers: HEADERS,
-      });
-      res.json(response.data);
-    } catch (error) {
-      res.status(500).send("Error fetching balances");
-    }
-  });
-}
+export function getHistoricalBalance() {}
