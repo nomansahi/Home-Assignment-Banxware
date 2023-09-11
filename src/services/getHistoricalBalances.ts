e23a66294b1df1ddc9a13efbeafcc30f4513f9e3
NOMAN ALI <87797326+nomansahi@users.noreply.github.com>
2023-09-11 16:50:45 +0200

    add provided api url and also provided API key value

--- src/services/getHistoricalBalances.ts
+++ src/services/getHistoricalBalances.ts
@@ -1,5 +1,13 @@
 // This is just an example, it is not expected from you to keep this function
 // signature or even the file name. Feel free to change whatever you like.
+
+import express = require("express");
+import axios = require("axios");
 export function getHistoricalBalance() {
-  return { hello: "world" };
+  const BASE_URl =
+    "https://uh4goxppjc7stkg24d6fdma4t40wxtly.lambda-url.eu-central-1.on.aws";
+  const HEADERS = {
+    "x-api-key":
+      "b4a4552e-1eac-44ac-8fcc-91acca085b98-f94b74ce-aa17-48f5-83e2-8b8c30509c18",
+  };
 }
